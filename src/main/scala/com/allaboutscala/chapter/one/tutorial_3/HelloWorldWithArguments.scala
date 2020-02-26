59ae267c8423c9d0c44a5cc1a42931686fbab3d8
ushapatturaj <ushadevipatturaj@gmail.com>
2020-02-26 17:21:40 +0100

    Hello World program

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_3/HelloWorldWithArguments.scala
@@ -0,0 +1,10 @@
+package com.allaboutscala.chapter.one.tutorial_3
+
+object HelloWorldWithArguments extends App {
+
+  println("Hello WOrld Program with Run time Arguments!")
+  println("The command line arguments are :")
+  println(args.mkString(", "))
+
+
+}
---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_4/HelloWorldWithArgumentsDebug.scala
@@ -0,0 +1,8 @@
+package com.allaboutscala.chapter.one.tutorial_4
+
+object HelloWorldWithArgumentsDebug extends App {
+
+  println("Hello WOrld Program with Run time Arguments!")
+  println("The command line arguments are :")
+  println(args.mkString(", "))
+}
