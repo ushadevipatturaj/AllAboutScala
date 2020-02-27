62dae5e3fcec6c17722fd9c2c1870ad1675169fa
ushapatturaj <ushadevipatturaj@gmail.com>
2020-02-27 14:14:43 +0100

    VariablesandTypes

--- src/main/scala/com/allaboutscala/chapter/two/tutorial_1/VariablesandTypes.scala
+++ src/main/scala/com/allaboutscala/chapter/two/tutorial_1/VariablesandTypes.scala
@@ -2,7 +2,7 @@ package com.allaboutscala.chapter.two.tutorial_1
 
 object VariablesandTypes extends App {
 val donutstobuy: Int=5
-  var favouritedonut:String ="Blaze Donut"
+  var favouritedonut:String ="Glaze Donut"
   println( "Donuts to buy :" + donutstobuy +"\nfavourite Donut :" + favouritedonut )
   lazy val leastfavourite="Plain Donut"
   var name:String =_
