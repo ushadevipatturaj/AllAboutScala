a38ce0c501d91fbcb7419315e8e144c62f02fee2
ushapatturaj <ushadevipatturaj@gmail.com>
2020-02-27 12:14:31 +0100

    Hello World program with Lazy logging

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_5/HelloWorldWithScalaLogging.scala
@@ -0,0 +1,7 @@
+package com.allaboutscala.chapter.one.tutorial_5
+import com.typesafe.scalalogging.LazyLogging
+object HelloWorldWithScalaLogging extends App with LazyLogging {
+
+logger.info("Hello World Program with Scala Logging")
+
+}
