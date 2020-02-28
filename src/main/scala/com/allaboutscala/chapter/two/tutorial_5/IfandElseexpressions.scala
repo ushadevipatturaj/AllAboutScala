07e8c54363719537d625ffadd0df0fa017ee7d7e
ushapatturaj <ushadevipatturaj@gmail.com>
2020-02-28 12:09:19 +0100

    If and Else Expressions in Scala

---
+++ src/main/scala/com/allaboutscala/chapter/two/tutorial_5/IfandElseexpressions.scala
@@ -0,0 +1,20 @@
+package com.allaboutscala.chapter.two.tutorial_5
+import scala.io.StdIn._
+object IfandElseexpressions extends App{
+val donutsperperson=2
+  val defaultdonuts=10
+  val totalpeople=readLine("No of people available: ").toInt
+
+  if(totalpeople>=10)
+    {
+      println("Donuts to buy :"+totalpeople*donutsperperson)
+    }
+else if (totalpeople ==0)
+    {
+      println("No person available")
+    }
+  else
+    {
+      println("Donuts to buy is :"+defaultdonuts)
+    }
+}
