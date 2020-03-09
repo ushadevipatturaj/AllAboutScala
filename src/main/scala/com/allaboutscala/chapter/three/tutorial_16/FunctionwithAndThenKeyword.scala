da4a5c1c1ac24a03213b06b167bc05929e449fa1
ushapatturaj <ushadevipatturaj@gmail.com>
2020-03-09 16:46:24 +0100

    Functioncall using andThen keyword

---
+++ src/main/scala/com/allaboutscala/chapter/three/tutorial_16/FunctionwithAndThenKeyword.scala
@@ -0,0 +1,17 @@
+package com.allaboutscala.chapter.three.tutorial_16
+
+object FunctionwithAndThenKeyword extends App{
+  val applycoupon=(totalcost:Double)=>{
+    val discount=1.45
+    totalcost-discount
+  }
+
+  val applytax = (totalcost:Double)=> {
+    val tax=0.75
+    totalcost+tax
+  }
+println("Step1:Calling the functions separately ")
+  println(s"Calling Coupon and tax functions ${applycoupon(17.00) } , ${applytax(17.00)}")
+  println("Step2:Calling the functions using AndThen Keyword ")
+  println(s"Calling Coupon and tax functions ${(applycoupon andThen applytax)(17.00)}")
+}
