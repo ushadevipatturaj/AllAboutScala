7037e3a9e7178bb228a3ea9d5d6ccf81ebaecffd
ushapatturaj <ushadevipatturaj@gmail.com>
2020-03-18 19:48:25 +0100

    Contra-variance in Type Class

---
+++ src/main/scala/com/allaboutscala/chapter/four/tutorial_14/ContraVarianceinTypeClass.scala
@@ -0,0 +1,40 @@
+package com.allaboutscala.chapter.four.tutorial_14
+
+object ContraVarianceinTypeClass extends App {
+    abstract class Donut(name:String){
+      def printname:Unit
+    }
+    println("Creating case sub classes for class Donut")
+    case class VanillaDonut (name:String) extends Donut(name){
+      override def printname: Unit = {
+        println(s"Donut name =$name")
+      }
+    }
+
+    case class GlazedDonut (name:String) extends Donut(name){
+      override def printname: Unit = {
+        println(s"Donut name =$name")
+      }
+    }
+    println("Creating objects for the subclasses of Donut")
+    val vanilla=VanillaDonut("Vanilla Donut")
+    val glazed=GlazedDonut("Glazed Donut")
+    vanilla.printname
+    glazed.printname
+
+    println("Creating typed classes of type Donut ")
+    // the typed class will not allow ay other type apart from Donut, for eg, we cannot create Seq[String]
+    class Shoppingcart[-D <: Donut](shoppingcart:Seq[D]){ // Shoppingcart[-D<: Donut] is the contra variance to allow creation of Donut object under vanilla donut class
+      def printcartitems = shoppingcart.foreach(donut => println(s"Donut name is :$donut"))
+    }
+    println("creating Seq with Donut objects and call Shopping cart class")
+    val donutsequence= Seq[Donut](vanilla,glazed)
+    val shoppingcartobj=new Shoppingcart[Donut](donutsequence)
+    val shoppingcartobj1:Shoppingcart[Donut]=new Shoppingcart[Donut](donutsequence)
+    val shoppingcartobj2:Shoppingcart[VanillaDonut] = new Shoppingcart[VanillaDonut](Seq(vanilla))
+    val shoppingcartobj3:Shoppingcart[VanillaDonut]=new Shoppingcart[Donut](Seq(vanilla)) //this will work only when the contra variance is enabled Shoppingcart[-D<: Donut]
+    shoppingcartobj.printcartitems
+    shoppingcartobj1.printcartitems
+    shoppingcartobj2.printcartitems
+    shoppingcartobj3.printcartitems
+}
