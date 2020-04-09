b8a96ad804d947f0ef077f751a20dfcbfae6a9b6
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-09 15:09:48 +0200

    Collection functions - diff

--- src/main/scala/com/allaboutscala/chapter/eight/tutorial_3/Functions_DIff.scala
+++ src/main/scala/com/allaboutscala/chapter/eight/tutorial_3/Functions_DIff.scala
@@ -12,4 +12,7 @@ object Functions_DIff extends App {
   val list2:List[String]=List("Vanilla Donut","Strawberry Donut")
   println(s"Difference of 2 Lists are ${list1 diff list2}")
   println(s"Difference of 2 List are ${list2 diff list1}")
+
+  println("Step2: Difference of 2 Lists using --")
+  println(s"Difference of 2 Lists are ${set1 -- set2}") //--can be applied only for the Set and not for other collections
 }
