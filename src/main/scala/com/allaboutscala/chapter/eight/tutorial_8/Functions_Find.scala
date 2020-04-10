1a63aa3e5943f12a480f6faf400977e419f80a7d
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-10 21:33:59 +0200

    Collection functions - Find

--- src/main/scala/com/allaboutscala/chapter/eight/tutorial_8/Functions_Find.scala
+++ src/main/scala/com/allaboutscala/chapter/eight/tutorial_8/Functions_Find.scala
@@ -2,7 +2,7 @@ package com.allaboutscala.chapter.eight.tutorial_8
 
 object Functions_Find extends App{
   println("Step1: Create a Seq ")
-  val seq1:Seq[String]=Seq("Plain Donut","Vanilla Donut","Strawberry Donut")
+  val seq1:Seq[String]=Seq("Plain Donut","Vanilla Donut","Strawberry Donut","Plain Donut")
   println(seq1)
   println("Step2: Applying find directly \n"+seq1.find(donut => donut.contains("Plain")))
 
