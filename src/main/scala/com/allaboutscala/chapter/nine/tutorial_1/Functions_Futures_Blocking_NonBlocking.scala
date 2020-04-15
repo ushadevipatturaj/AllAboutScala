056ac8667f7f4132bd484069d74dfab57c12cf3f
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-15 20:00:49 +0200

    Futures - Introduction,Blocking and Non-Blocking call of Future method, Success and Failure functions

--- src/main/scala/com/allaboutscala/chapter/nine/tutorial_1/Functions_Futures_Blocking_NonBlocking.scala
+++ src/main/scala/com/allaboutscala/chapter/nine/tutorial_1/Functions_Futures_Blocking_NonBlocking.scala
@@ -1,10 +1,12 @@
 package com.allaboutscala.chapter.nine.tutorial_1
 import org.joda.time.Seconds
+
 import scala.language._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.util.{Failure,Success}
 object Functions_Futures_Blocking_NonBlocking extends App {
   println("Step1: Creating a method with return type of Future")
   def checkStock(donut:String):Future[Int]=Future{
@@ -17,4 +19,11 @@ object Functions_Futures_Blocking_NonBlocking extends App {
 
   println("Step3: Calling a method with non-blocking .onComplete")
   checkStock("Vanilla Donut").onComplete(stock => println(stock))
+
+  println("Step4: Calling a method with non-blocking using Success and Failure")
+  checkStock("Glazed Donut").onComplete{
+    case Success(stock) => println(s"Stock for Glazed Donut is $stock")
+    case Failure(stock) => println("No Stock is available!")
+  }
+  Thread.sleep(3000)
 }
---
+++ src/main/scala/com/allaboutscala/chapter/nine/tutorial_2/Futures_Chaining_Futures.scala
@@ -0,0 +1,5 @@
+package com.allaboutscala.chapter.nine.tutorial_2
+
+object Futures_Chaining_Futures {
+
+}
