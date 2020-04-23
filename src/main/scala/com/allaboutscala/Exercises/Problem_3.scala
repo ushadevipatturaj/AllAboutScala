da0e5833981e8b5f95f3277ab6c90b22789ff991
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-23 12:30:03 +0200

    Exercises - Problem 3-Getting input from Console and changing console printing properties

---
+++ src/main/scala/com/allaboutscala/Exercises/Problem_3.scala
@@ -0,0 +1,19 @@
+package com.allaboutscala.Exercises
+import scala.io.StdIn._
+object Problem_3 extends App {
+  val name=readLine("Enter the Name: ")
+  println("Enter the Age")
+  val age=readInt()
+  println(Console.BOLD)
+  print("Name :" )
+  print(Console.RESET)
+  print(Console.UNDERLINED)
+  print(name)
+  print(Console.RESET)
+  println(Console.BOLD)
+  print("Age: ")
+  print(Console.RESET)
+  print(Console.UNDERLINED)
+  print(age)
+
+}
