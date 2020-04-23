9643a37d62c3c66816d823389de40b34531173de
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-23 13:26:35 +0200

    Exercises - Problem 6- Multi line printing

---
+++ src/main/scala/com/allaboutscala/Exercises/Problem_6.scala
@@ -0,0 +1,9 @@
+package com.allaboutscala.Exercises
+import scala.io.StdIn._
+object Problem_6 extends App {
+  val favouriteMovie=readLine("Enter the favourite movie: ")
+  println(s"""
+      |"$favouriteMovie" is awesome!
+      |""".stripMargin)
+
+}
