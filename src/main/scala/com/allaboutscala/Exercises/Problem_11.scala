de102047dc3433b25082f9e764b194547132bd81
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-23 14:16:28 +0200

    Exercises - Problem 11-Filternot and Applying Mkstring with start,sep,end

--- src/main/scala/com/allaboutscala/Exercises/Problem_11.scala
+++ src/main/scala/com/allaboutscala/Exercises/Problem_11.scala
@@ -2,7 +2,7 @@ package com.allaboutscala.Exercises
 
 object Problem_11 extends App {
   val cartItems:Set[String]=Set( "Milk", "Cheese", "Donuts", "Apples", "Bananas")
-  val ValueFunction:(String)=>Boolean=(item)=> (item =="Bananas" || item =="Apples")
+  val ValueFunction: String =>Boolean= item=> item =="Bananas" || item =="Apples"
   val noFruits=cartItems.filterNot(ValueFunction)
 
   println(noFruits.mkString("<items values= \"","|","\"></items>"))
