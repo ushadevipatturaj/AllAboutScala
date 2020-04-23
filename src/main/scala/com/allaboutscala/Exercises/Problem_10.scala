dfbc4039d4ef1cfe2dbdce91f1450524fff08cb4
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-23 13:58:36 +0200

    Exercises - Problem 10- Finding the common items from the 2 baskets

---
+++ src/main/scala/com/allaboutscala/Exercises/Problem_10.scala
@@ -0,0 +1,9 @@
+package com.allaboutscala.Exercises
+
+object Problem_10 extends App {
+  val basketOne:Set[String] = Set("Cake","Milk", "Cheese" , "ToiletPaper")
+  val basketTwo:Set[String] = Set("Water","Milk", "Juice" , "Cheese","Bread")
+  val commonItems=basketOne & basketTwo
+  println(s"Getting the common elements from two baskets \n${commonItems.mkString("\n").toUpperCase}")
+
+}
