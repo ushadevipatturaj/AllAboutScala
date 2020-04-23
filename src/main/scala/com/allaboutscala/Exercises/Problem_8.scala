33b8046f8591d6e23b2795be22a5ae1d87e66d59
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-23 13:37:53 +0200

    Exercises - Problem 8-Split the String

---
+++ src/main/scala/com/allaboutscala/Exercises/Problem_8.scala
@@ -0,0 +1,13 @@
+package com.allaboutscala.Exercises
+
+object Problem_8 extends App {
+  val str="Vanilla Donut 10 2.25"
+  val splitString=str.split(" ")
+  println(
+    s"""
+       |Donut Name: ${splitString(0)} ${splitString(1)}
+       |Quantity : ${splitString(2).toInt}
+       |Price : ${splitString(3).toDouble}
+       |""".stripMargin)
+
+}
