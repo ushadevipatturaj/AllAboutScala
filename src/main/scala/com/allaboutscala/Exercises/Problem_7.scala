da26617528d87cfa0dc7f209378e95ed688a450e
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-23 13:27:16 +0200

    Exercises - Problem 7- Multiline printing

---
+++ src/main/scala/com/allaboutscala/Exercises/Problem_7.scala
@@ -0,0 +1,16 @@
+package com.allaboutscala.Exercises
+import scala.io.StdIn._
+object Problem_7 extends App {
+  val name=readLine("Enter the Name: ")
+  println("Enter the Age: ")
+  val age=readInt()
+  val movie=readLine("Enter the favourite movie: ")
+  println(
+    s"""
+      |First Name: $name
+      |Age : $age
+      |Favourite Movie : $movie
+      |""".stripMargin)
+
+
+}
