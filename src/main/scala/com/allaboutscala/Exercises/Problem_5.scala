7dd77a451ad7f8d2af31ca6dfba3fd11cffd6768
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-23 13:06:41 +0200

    Exercises - Problem 4- printing $ and 2floating points

---
+++ src/main/scala/com/allaboutscala/Exercises/Problem_5.scala
@@ -0,0 +1,10 @@
+package com.allaboutscala.Exercises
+
+object Problem_5 extends App {
+
+  val qty=10
+  val price=2.50
+  val totalAmt=qty*price
+  println(f"""Total cost of 10 Glazed Donuts = $$$totalAmt%.2f""")
+
+}
