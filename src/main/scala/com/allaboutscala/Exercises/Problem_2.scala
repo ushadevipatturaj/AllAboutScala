5c653a1bca589dbe806b2bd44b10354104c6783a
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-23 12:19:11 +0200

    Exercises - Problem 2- Multi Line String printing

---
+++ src/main/scala/com/allaboutscala/Exercises/Problem_2.scala
@@ -0,0 +1,13 @@
+package com.allaboutscala.Exercises
+
+object Problem_2 extends App {
+  val string=
+    """
+      |{
+      |"donut_name":"Vanilla Donut",
+      |"quantity_purchased":"10",
+      |"price":2.5
+      |}
+      |""".stripMargin
+  println(string)
+}
