1f2c58d519509f74f88b846d0544b0a224cce6fd
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-23 14:40:41 +0200

    Exercises - Problem 13- capitalizing a string "T-Shirt" of tuple3

---
+++ src/main/scala/com/allaboutscala/Exercises/Problem_13.scala
@@ -0,0 +1,15 @@
+package com.allaboutscala.Exercises
+
+object Problem_13 extends App {
+  val tupleList:List[(String,String,Double)] = List(
+    ("T-Shirt", "Medium", 10.99),
+    ("Polo-Shirt", "Large", 4.99),
+    ("Vest", "Small", 5.99),
+    ("T-Shirt", "Small", 4.99),
+    ("T-Shirt", "Small", 4.99)
+  )
+  tupleList.foreach{
+    case str if str._1 =="T-Shirt" => println(s"${str._1.toUpperCase} is price at $$${str._3} for the ${str._2} size.")
+    case str => println(s"${str._1.toLowerCase()} is price at $$${str._3} for the ${str._2} size.")
+  }
+}
