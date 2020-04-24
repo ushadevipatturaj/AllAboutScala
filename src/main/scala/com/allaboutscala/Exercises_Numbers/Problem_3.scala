977ff3160489c7a6f13c8f75ec1a2bad508baff8
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-24 12:29:01 +0200

    Problem 3- Getting the Largest and Smallest value from the List

---
+++ src/main/scala/com/allaboutscala/Exercises_Numbers/Problem_3.scala
@@ -0,0 +1,8 @@
+package com.allaboutscala.Exercises_Numbers
+
+object Problem_3 extends App{
+  val numSeq:Seq[Int]=Seq(0, 10, 20, 47, -2, 99, -98)
+  println("The smallest item in the Vector =" + numSeq.min)
+  println("The largest item in the Vector = " + numSeq.max)
+
+}
